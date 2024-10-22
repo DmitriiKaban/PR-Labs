2da050b825612332deb50dbe79039d0899298865
Cravcenco Dmitrii <dima16kravchenko@gmail.com>
2024-10-22 13:31:51 +0300

    Add GET controller

---
+++ Lab 2/src/main/java/com/app/lab2/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.app.lab2.controllers;
+
+import com.app.lab2.models.Product;
+import com.app.lab2.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public String showProducts(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "products";
+    }
+}
